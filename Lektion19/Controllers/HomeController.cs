9f07579358377094e1c6c36798b0b39e0e47fb83
David Gullmarsvik <awt2.2012@gmail.com>
2012-10-01 11:24:14 +0200

    Uppgift 3

--- Lektion19/Controllers/HomeController.cs
+++ Lektion19/Controllers/HomeController.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNetOpenAuth.ApplicationBlock;
+using System.Configuration;
 
 namespace Lektion19.Controllers
 {
     public class HomeController : Controller
     {
+        private string AccessToken
+        {
+            get { return (string)Session["TwitterAccessToken"]; }
+            set { Session["TwitterAccessToken"] = value; }
+        }
+        private InMemoryTokenManager TokenManager
+        {
+            get
+            {
+                var tokenManager = (InMemoryTokenManager)HttpContext.Application["TwitterTokenManager"];
+                if (tokenManager == null)
+                {
+                    string consumerKey = ConfigurationManager.AppSettings
+                    ["twitterConsumerKey"];
+                    string consumerSecret = ConfigurationManager.AppSettings
+                    ["twitterConsumerSecret"];
+                    if (!string.IsNullOrEmpty(consumerKey))
+                    {
+                        tokenManager = new InMemoryTokenManager(consumerKey,
+                        consumerSecret);
+                        HttpContext.Application["TwitterTokenManager"] =
+                        tokenManager;
+                    }
+                }
+                return tokenManager;
+            }
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
